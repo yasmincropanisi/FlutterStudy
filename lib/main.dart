fc49a5ecb0452f6e72509cea90f71bbefbfdbaee
Yasmin Cropanisi <yasmin.cropanisi@arctouch.com>
2020-01-29 15:27:16 -0300

    Random buttons with colors - lesson 78

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
+import 'dart:math';
 
 void main() => runApp(XylophoneApp());
 
@@ -8,9 +10,42 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: SafeArea(
+            child: Center(
+                child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                ..._playableButtons(total: 7),
+              ],
+            )),
+          ),
         ),
       ),
     );
+  }
+
+  List<FlatButton> _playableButtons({total: int}) {
+    var buttons = List<FlatButton>();
+    for (int i = 1; i <= total; i++) {
+      buttons.add(
+        FlatButton(
+          shape: RoundedRectangleBorder(
+            side: BorderSide.none,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          color: Color((Random().nextDouble() * 0xFFFFFF).toInt() << 0)
+              .withOpacity(1.0),
+          onPressed: () {
+            final player = AudioCache();
+            player.play('note$i.wav');
+          },
+          child: Text(
+            'Click me',
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+      );
+    }
+    return buttons;
   }
 }
